5300d0dd918729bcb6b3882523dbefa06613caa8
Boris Nikolic <borisnikolic2302@gmail.com>
2025-01-21 21:35:48 +0100

    C8 task1  completed

--- Operating-Systems/homework/OSTEP/C8/main.cpp
+++ Operating-Systems/homework/OSTEP/C8/main.cpp
@@ -19,8 +19,56 @@
 // level (with the -B flag) in order to guarantee that a single long-
 // running (and potentially-starving) job gets at least 5% of the CPU?
 
+// mlfq is priority queue scheduler
+// mlfq has queues based on priorities
+// and we are pushing the jobs based on the priority of the job
 
-int int main (int argc, char *argv[]) {
-  
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+struct Job {
+  int id;
+  int arrival_time;
+  int elapsed_time;
+  int remaining_time;
+  int priority;
+
+  bool operator<(const Job &other) const { return priority > other.priority; }
+};
+
+void generate_jobs(std::queue<Job> queues[]) {
+  for (int i = 0; i < 2; i++) {
+    Job new_job;
+    new_job.id = i;
+    new_job.priority = i;
+    new_job.arrival_time = std::time(0);
+    new_job.elapsed_time = 0;
+    new_job.remaining_time = 10 + (std::rand() % 91);
+
+    queues[i].push(new_job);
+  }
+}
+
+void task_1() {
+  std::queue<Job> queues[2];
+  generate_jobs(queues);
+
+  for (int i = 0; i < 2; i++) {
+    while (!queues[i].empty()) {
+      Job job = queues[i].front();
+      queues[i].pop();
+      std::cout << "Job ID: " << job.id << ", Priority: " << job.priority
+                << ", Arrival Time: " << job.arrival_time
+                << ", Remaining Time: " << job.remaining_time << "ms\n";
+    }
+  }
+}
+
+int main() {
+  std::srand(std::time(0));
+  task_1();
   return 0;
 }
